169a2decbc52c91dbab07b861be11e94b9f013a3
christina-yun <poncheeyun@yahoo.com>
2021-09-30 09:44:12 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+console.log('web46 rules!');
+console.log(process.argv[2]);
+console.log(process.argv[3]);
+console.log(process.env.USER);
+console.log(process.env.FOO);
+
+const express = require('express');
+
+const server = express();
+server.use(express.json());
+
+server. get('/hello', (req, res) => {
+    res.send('<h1> HELLO THERE</h1>');
+});
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+    console.log(`listening on ${port}`);
+});
